c00fc36f43e421f8cc151063eb91d7225b16a741
Azmi FH <azmialhaq17@gmail.com>
2025-04-21 02:23:43 +0700

    button 1 done

--- Intent/app/build.gradle.kts
+++ Intent/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.intent"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.intent"
--- Intent/app/src/main/java/com/example/intent/MainActivity.kt
+++ Intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.intent
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnpindah : Button = findViewById(R.id.btn_move)
+        btnpindah.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_move ->{
+                val moveIntent = Intent(this@MainActivity,MoveActivity::class.java)
+                startActivity(moveIntent)
+
+            }
         }
     }
 }
